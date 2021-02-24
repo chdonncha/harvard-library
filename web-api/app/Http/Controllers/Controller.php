f736e58c6aa3b9b437b54232f99e0f93a4e182de
chdonncha@gmail.com <T3CUmI4GNUa1>
2021-02-24 00:06:56 +0000

    Setup book pull from harvard and insert data to local db

--- web-api/app/Http/Controllers/Controller.php
+++ web-api/app/Http/Controllers/Controller.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\Library;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    function test()
+    public static function getBooks()
     {
-        echo "hello there";
+        $client = new Client();
+        $request = $client->get('http://api.lib.harvard.edu/v2/collections');
+        $response = $request->getBody()->getContents();
+
+        Library::addBooks($response);
     }
 }
--- web-api/app/Models/Library.php
+++ web-api/app/Models/Library.php
@@ -7,5 +7,51 @@ use Illuminate\Database\Eloquent\Model;
 
 class Library extends Model
 {
+    protected $table = 'Books';
+
+    protected $fillable =
+        [
+            'created',
+            'modified',
+            'baseUrl',
+            'collectionUrn',
+            'contactDepartment',
+            'contactName',
+            'dcp',
+            'systemId',
+            'public',
+            'setDescription',
+            'setName',
+            'setSpec',
+            'thumbnailUrn',
+        ];
+
     use HasFactory;
+
+    public static function addBooks($request)
+    {
+        $dataSet = [];
+        $request = json_decode($request);
+
+        foreach($request as $book)
+        {
+            $dataSet[] = [
+                'created'           => $book->created,
+                'modified'          => $book->modified,
+                'baseurl'           => $book->baseUrl,
+                'collectionUrn'     => $book->collectionUrn,
+                'contactDepartment' => $book->contactDepartment,
+                'contactName'       => $book->contactName,
+                'dcp'               => $book->dcp,
+                'systemId'          => $book->systemId,
+                'public'            => $book->public,
+                'setDescription'    => $book->setDescription,
+                'setName'           => $book->setName,
+                'setSpec'           => $book->setSpec,
+                'thumbnailUrn'      => $book->thumbnailUrn,
+                ];
+        }
+
+        Library::insert($dataSet);
+    }
 }
